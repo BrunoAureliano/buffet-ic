1ccd3251fb6a483421309a448f3f90444316c34d
Bruno Henrique Aureliano <brunoaureliano2006@gmail.com>
2026-08-26 18:08:16 -0300

    feat: rota de login e validacao de credenciais

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -32,3 +32,47 @@ export const cadastrarUsuario = async (req, res) => {
         res.status(500).json({ erro: 'Falha ao processar a segurança da senha.' });
     }
 };
+
+
+export const loginUsuario = async (req, res) => {
+    const { email, senha } = req.body;
+
+    const sql = 'SELECT * FROM Usuario WHERE email = ?';
+
+    db.query(sql, [email], async (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar usuário:', err);
+            return res.status(500).json({ erro: 'Falha na comunicação com o banco.' });
+        }
+
+        // Verifica se encontrou algum usuário com esse e-mail
+        if (results.length === 0) {
+            return res.status(401).json({ erro: 'E-mail ou senha incorretos.' });
+        }
+
+        const usuarioEncontrado = results[0];
+
+        try {
+            // Compara a senha digitada com a senha criptografada do banco
+            const senhaValida = await bcrypt.compare(senha, usuarioEncontrado.senha);
+
+            if (!senhaValida) {
+                return res.status(401).json({ erro: 'E-mail ou senha incorretos.' });
+            }
+
+            res.status(200).json({
+                mensagem: 'Login realizado com sucesso!',
+                usuario: {
+                    id: usuarioEncontrado.idUsuario,
+                    nome: usuarioEncontrado.nome,
+                    email: usuarioEncontrado.email,
+                    perfil: usuarioEncontrado.perfil
+                }
+            });
+
+        } catch (erro) {
+            console.error('Erro na validação da senha:', erro);
+            res.status(500).json({ erro: 'Erro ao validar o acesso.' });
+        }
+    });
+};
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { cadastrarUsuario } from '../controllers/usuarioController.js';
+import { cadastrarUsuario, loginUsuario } from '../controllers/usuarioController.js';
 
 const router = express.Router();
 
 router.post('/cadastro', cadastrarUsuario);
+router.post('/login', loginUsuario);
 
 export default router;
